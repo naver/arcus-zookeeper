f417ae24b1b6d33aa0bdd36885bd14c1c6e0b98b
Hoonmin Kim <harebox@gmail.com>
2014-02-10 13:42:33 +0900

    C library: prefix LOG macros to avoid name conflicts
    LOG_ERROR and so on defined in zookeeper_log.h are pretty common names.
    ZooKeeper client programs might already use them.  So to avoid conflicts, such
    programs should define ZOOKEEPER_C_CLIENT before including zookeeper_log.h.
    The macros then start with prefix ZOO_.  LOG_ERROR becomes ZOO_LOG_ERROR and
    so on.
    
    Apply this patch to 3.4.5, in preparation for open source Arcus.

--- src/c/include/zookeeper_log.h
+++ src/c/include/zookeeper_log.h
@@ -28,14 +28,24 @@ extern "C" {
 extern ZOOAPI ZooLogLevel logLevel;
 #define LOGSTREAM getLogStream()
 
-#define LOG_ERROR(x) if(logLevel>=ZOO_LOG_LEVEL_ERROR) \
+#define ZOO_LOG_ERROR(x) if(logLevel>=ZOO_LOG_LEVEL_ERROR) \
     log_message(ZOO_LOG_LEVEL_ERROR,__LINE__,__func__,format_log_message x)
-#define LOG_WARN(x) if(logLevel>=ZOO_LOG_LEVEL_WARN) \
+#define ZOO_LOG_WARN(x) if(logLevel>=ZOO_LOG_LEVEL_WARN) \
     log_message(ZOO_LOG_LEVEL_WARN,__LINE__,__func__,format_log_message x)
-#define LOG_INFO(x) if(logLevel>=ZOO_LOG_LEVEL_INFO) \
+#define ZOO_LOG_INFO(x) if(logLevel>=ZOO_LOG_LEVEL_INFO) \
     log_message(ZOO_LOG_LEVEL_INFO,__LINE__,__func__,format_log_message x)
-#define LOG_DEBUG(x) if(logLevel==ZOO_LOG_LEVEL_DEBUG) \
+#define ZOO_LOG_DEBUG(x) if(logLevel==ZOO_LOG_LEVEL_DEBUG) \
     log_message(ZOO_LOG_LEVEL_DEBUG,__LINE__,__func__,format_log_message x)
+
+/* LOG_ is common naming.  So, when compiling a client program, prefix these
+ * macros with ZOO_ to avoid conflicts.
+ */
+#ifndef ZOOKEEPER_C_CLIENT
+#define LOG_ERROR  ZOO_LOG_ERROR
+#define LOG_WARN   ZOO_LOG_WARN
+#define LOG_INFO   ZOO_LOG_INFO
+#define LOG_DEBUG  ZOO_LOG_DEBUG
+#endif
 
 ZOOAPI void log_message(ZooLogLevel curLevel, int line,const char* funcName,
     const char* message);
