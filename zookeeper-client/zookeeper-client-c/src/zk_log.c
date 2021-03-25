c8eb6e07fc5b5827a69abc682e454e5ba9fcc38c
Hoonmin Kim <harebox@gmail.com>
2021-03-25 12:08:17 +0900

    C library: prefix LOG macros to avoid name conflicts
    LOG_ERROR and so on defined in zookeeper_log.h are pretty common names.
    ZooKeeper client programs might already use them.  So to avoid conflicts, such
    programs should define ZOOKEEPER_C_CLIENT before including zookeeper_log.h.
    The macros then start with prefix ZOO_.  LOG_ERROR becomes ZOO_LOG_ERROR and
    so on.
    
    Apply this patch to 3.4.5, in preparation for open source Arcus.

--- zookeeper-client/zookeeper-client-c/src/zk_log.c
+++ zookeeper-client/zookeeper-client-c/src/zk_log.c
@@ -189,6 +189,66 @@ void log_message(log_callback_fn callback, ZooLogLevel curLevel,
     }
 }
 
+#ifdef ARCUS_C_CLIENT_LOG
+void zoo_log_message(ZooLogLevel curLevel,int line,const char* funcName,
+    const char* message)
+{
+    static const char* dbgLevelStr[]={"ZOO_INVALID","ZOO_ERROR","ZOO_WARN",
+            "ZOO_INFO","ZOO_DEBUG"};
+    static pid_t pid=0;
+#ifdef THREADED
+    unsigned long int tid = 0;
+#endif
+#ifdef WIN32
+    char timebuf [TIME_NOW_BUF_SIZE];
+    const char* time = time_now(timebuf);
+#else
+    const char* time = time_now(get_time_buffer());
+#endif
+
+    if(pid==0)
+    {
+        pid=getpid();
+    }
+
+    if(enableSyslog && logLevel >= curLevel) {
+        static const char* sysDbgLevelStr[]={"INVALID","ERROR","WARN",
+                                             "INFO","DEBUG"};
+        syslog(map_syslog_priority(curLevel), "zookeeper[%s@%s@%d] %s",
+               sysDbgLevelStr[curLevel], funcName, line, message);
+        return;
+    }
+
+#ifndef THREADED
+    fprintf(zoo_get_log_stream(), "%s:%ld:%s@%s@%d: %s\n", time, (long)pid,
+            dbgLevelStr[curLevel],funcName,line,message);
+#else
+    #ifdef WIN32
+        tid = (unsigned long int)(pthread_self().thread_id);
+    #else
+        tid = (unsigned long int)(pthread_self());
+    #endif
+
+    fprintf(zoo_get_log_stream(), "%s:%ld(0x%lx):%s@%s@%d: %s\n", time, (long)pid, tid,
+            dbgLevelStr[curLevel], funcName, line, message);
+#endif
+    fflush(zoo_get_log_stream());
+}
+
+const char* format_log_message(const char* format,...)
+{
+    va_list va;
+    char* buf=get_format_log_buffer();
+    if(!buf)
+        return "format_log_message: Unable to allocate memory buffer";
+
+    va_start(va,format);
+    vsnprintf(buf, FORMAT_LOG_BUF_SIZE-1,format,va);
+    va_end(va);
+    return buf;
+}
+#endif
+
 void zoo_set_debug_level(ZooLogLevel level)
 {
     if(level==0){
